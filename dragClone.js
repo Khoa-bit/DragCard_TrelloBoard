d760bd4571ccd0f31686c1b05d7b063f86f05b0c
Khoa-Bit <55542012+Khoa-bit@users.noreply.github.com>
2021-11-11 17:36:13 +0700

    feat: ✨ implement drag clone

--- dragClone.js
+++ dragClone.js
@@ -1,126 +1,54 @@
 "use strict";
 
 let board = document.querySelector("div.board");
+let dragged;
 
 board.ondragstart = function (event) {
-  event.preventDefault();
   let card = event.target.closest(".card");
 
   if (!card || !board.contains(card)) return;
 
-  let shiftX = event.clientX - card.getBoundingClientRect().left;
-  let shiftY = event.clientY - card.getBoundingClientRect().top;
-
-  moveAt(event.pageX, event.pageY);
-  card.style.position = "absolute";
-  card.style.opacity = "0.5";
-
-  card.classList.add("dragging");
-
-  // board.classList.toggle("mouseDrag");
-
-  function moveAt(pageX, pageY) {
-    card.style.left = pageX - shiftX + "px";
-    card.style.top = pageY - shiftY + "px";
-  }
-
-  // potential droppable that we're flying over right now
   let currentDroppable = null;
-  let isBelow = null;
+  
+  function onDragEnter(event) {
+    // highlight potential drop target when the draggable element enters it
+    if (!event.target) return;
 
-  function onMouseMove(event) {
-    moveAt(event.pageX, event.pageY);
+    let droppableBelow = event.target.closest(".droppable");
+    console.log("Enter");
+    console.log(droppableBelow);
 
-    card.style.display = "none";
-    let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
-    card.style.display = null;
-
-    // mousemove events may trigger out of the window (when the ball is dragged off-screen)
-    // if clientX/clientY are out of the window, then elementFromPoint returns null
-    if (!elemBelow || !board.contains(elemBelow)) return;
-
-    // potential droppables are labeled with the class "droppable" (can be other logic)
-    let droppableBelow = elemBelow.closest(".droppable");
-
-    if (currentDroppable != droppableBelow) {
-      // we're flying in or out...
-      // note: both values can be null
-      //   currentDroppable=null if we were not over a droppable before this event (e.g over an empty space)
-      //   droppableBelow=null if we're not over a droppable now, during this event
-
-      if (currentDroppable) {
-        // the logic to process "flying out" of the droppable (remove highlight)
-        currentDroppable.classList.remove("top-indicator", "bottom-indicator");
-        isBelow = null;
-      }
-      currentDroppable = droppableBelow;
-      if (currentDroppable) {
-        // the logic to process "flying in" of the droppable
-        updateIndicator(event);
-      }
-    } else if (currentDroppable != null) {
-      // the logic to process "moving in" of the droppable
-      updateIndicator(event);
-    }
+    currentDroppable = droppableBelow;
+    if (!droppableBelow) return;
+    droppableBelow.style.backgroundColor = "purple";
   }
 
-  function updateIndicator(event) {
-    let currentDroppableRect = currentDroppable.getBoundingClientRect();
-    let relativeY = event.clientY - currentDroppableRect.top;
+  function onDragLeave(event) {
+    // reset background of potential drop target when the draggable element leaves it
+    if (!event.target) return;
 
-    if (relativeY > currentDroppableRect.height / 2) {
-      if (isBelow == null || isBelow == false) {
-        isBelow = true;
-        currentDroppable.classList.remove("top-indicator");
-        currentDroppable.classList.add("bottom-indicator");
-      }
-    } else {
-      if (isBelow == null || isBelow == true) {
-        isBelow = false;
-        currentDroppable.classList.remove("bottom-indicator");
-        currentDroppable.classList.add("top-indicator");
-      }
-    }
+    let droppableBelow = event.target.closest(".droppable");
+    console.log("Leave");
+    console.log(droppableBelow);
+
+    if (!droppableBelow) return;
+    if (currentDroppable == droppableBelow) return;
+    droppableBelow.style.backgroundColor = null;
   }
 
-  function onMouseUp(event) {
-    if (currentDroppable) {
-      currentDroppable.classList.remove("top-indicator", "bottom-indicator");
-      if (isBelow) {
-        currentDroppable.after(card);
-      } else {
-        currentDroppable.before(card);
-      }
-      currentDroppable = null;
-      isBelow = null;
-    }
+  function onDragEnd(event) {
+    console.log("DragEnd");
+    console.log(event.target);
+    document.removeEventListener("dragenter", onDragEnter);
 
-    card.style.position = null;
-    card.style.opacity = null;
-    card.style.left = null;
-    card.style.top = null;
-    setTimeout(() => card.classList.remove("dragging"), 500);
-    // board.classList.toggle("mouseDrag");
-    document.removeEventListener("mousemove", onMouseMove);
-    document.removeEventListener("mouseup", onMouseUp);
+    document.removeEventListener("dragleave", onDragLeave);
+  
+    document.removeEventListener("dragend", onDragEnd);
   }
 
-  document.addEventListener("mousemove", onMouseMove);
-  document.addEventListener("mouseup", onMouseUp);
-};
+  document.addEventListener("dragenter", onDragEnter);
 
-board.onclick = function (event) {
-  // Prevent click event after dragend event.
-  let draggingElement = event.target.closest(".dragging");
-  if (draggingElement) {
-    draggingElement.classList.remove("dragging");
-    return;
-  }
+  document.addEventListener("dragleave", onDragLeave);
 
-  if (event.target.nodeName != "A") return;
-
-  let href = event.target.innerHTML;
-  alert(href); // ...can be loading from the server, UI generation etc
-
-  return false; // prevent browser action (don't go to the URL)
+  document.addEventListener("dragend", onDragEnd);
 };
